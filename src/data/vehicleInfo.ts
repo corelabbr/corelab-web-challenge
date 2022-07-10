bbe9f03cad68c29b7183f69176ff4dea3a3492fe
Neto <ss.neto88@gmail.com>
2022-07-10 06:44:40 -0300

    feat(AddVehicle): input generation data

---
+++ src/data/vehicleInfo.ts
@@ -0,0 +1,24 @@
+const vehicleInfo = [
+  {
+    name: "Nome",
+    inputAlias: "name",
+  },
+  {
+    name: "Marca",
+    inputAlias: "description",
+  },
+  {
+    name: "Cor",
+    inputAlias: "color",
+  },
+  {
+    name: "Ano",
+    inputAlias: "year",
+  },
+  {
+    name: "Placa",
+    inputAlias: "plate",
+  },
+];
+
+export default vehicleInfo;
