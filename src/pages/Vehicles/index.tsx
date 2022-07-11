ae82f1877d6ca1a1e2b3be75e648c9b46e869039
Neto <ss.neto88@gmail.com>
2022-07-11 11:47:08 -0300

    refactor(Vehicles Page): refactoring and absolute path implementation

--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import { getVehicles } from "../../lib/api";
-import { Button, Card, Search } from "../../components";
-import styles from "./Vehicles.module.scss";
 import { IVehicle } from "../../types/Vehicle";
+
+import FilterVehicle from "../FilterVehicle";
+import AddVehicle from "../AddVehicle";
+import { Button, Card, Search, FilterButton } from "@/components";
+import { vehicleInfo, filterVehicle, priceVehicle } from "../../data";
+
+import styles from "./Vehicles.module.scss";
+
+// TODO: implementar o fetch
 
 const VehiclesPage = () => {
   const [vehicles, setVehicles] = useState<IVehicle[]>([]);
@@ -17,23 +24,25 @@ const VehiclesPage = () => {
   //   fetchVehicles();
   // }, []);
 
-  // console.log({ vehicles });
+  console.log(vehicleInfo);
 
   return (
     <div className={styles.Vehicles}>
       <main className={styles.main}>
-        <div style={{ display: "flex" }}>
-          <Search placeholder='Buscar' value={search} onChange={(e: any) => setSearch(e)} />
-          <div style={{ width: "54px", height: "39px" }}>t</div>
+        <div className={styles.container}>
+          <Search placeholder='Buscar' value={search} onChange={(e) => {}} />
+          <FilterButton />
         </div>
 
         <Button text='Adicionar' onClick={() => {}} />
-
+        <AddVehicle data={vehicleInfo} />
+        <FilterVehicle characFilter={filterVehicle} priceFilter={priceVehicle} />
+        {/* 
         <Card title='Sandero Stepway'>
           <p>Price: 22000</p>
           <p>Description: Carro usado por 2 anos...</p>
           <p>Year: 2018</p>
-        </Card>
+        </Card> */}
       </main>
     </div>
   );
