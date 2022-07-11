c3fc4b3e37db848b4131daf4f96bf4190cc3c1ef
Neto <ss.neto88@gmail.com>
2022-07-11 11:49:08 -0300

    feat(FilterButton): filter implementation

---
+++ src/components/FilterButton/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilterButton = () => {
+  return <img src='./assets/config.svg' alt='configurações' style={{ width: "54px", height: "39px" }} />;
+};
+
+export default FilterButton;
