bb9ae710907d1f385e0ab74117eafe33a11f09ef
Kess220 <prodvictorkaio@gmail.com>
2024-01-22 19:31:25 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/main.tsx'), // ou 'src/main.ts' se for TypeScript
-      },
+      "@": "/src",
     },
   },
 });
