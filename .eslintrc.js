0e2ffce764aa308bd8da539779544163b9733825
MarcoAurelioDeFaria <marcoaureliodefariar@gmail.com>
2024-01-24 11:43:17 -0300

    Config eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard-with-typescript", "plugin:react/recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "no-console": 2,
+    "prettier/prettier": 2,
+  },
+};
